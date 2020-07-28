c7326d67e75549e47206e9a909dfeb7fa81d08b5
Tejas Parmar <tejasparmar99@gmail.com>
2020-07-28 22:35:02 +0530

    Added merge sort

--- Sorting/SortingAlgorithms.cpp
+++ Sorting/SortingAlgorithms.cpp
@@ -97,13 +97,58 @@ int QuickSort(int arr[], int start, int end){
 	return 0;
 }
 
+// Merge Sort
+// merge function
+void merge(int arr[], int l, int m, int r){
+	int n1 = m-l+1;
+	int n2 = r-m;	
+	int left[n1];
+	int right[n2];
+	for(int i=0; i<n1; i++)
+		left[i] = arr[l+i];
+	for(int i=0; i<n2; i++)
+		right[i] = arr[m+i+1];
+	int i = 0;
+	int j = 0;
+	int k = l;
+	while(i<n1 && j <n2){
+		if(left[i] <= right[j]){
+			arr[k++] = left[i++];
+		}
+		else if(left[i] > right[j]){
+			arr[k++] = right[j++];
+		}
+	}
+
+	while(i<n1){
+		arr[k++] = left[i++];
+	}
+
+	while(j<n2){
+		arr[k++] = right[j++];
+	}
+
+}
+// sort function
+void MergeSort(int arr[], int l, int r){
+	if(l<r){
+		int m = (l+r)/2;
+
+		MergeSort(arr,l,m);
+		MergeSort(arr,m+1,r);
+
+		merge(arr,l,m,r);
+	}
+}
+
+
 int main()
  {
 	//code
   	int arr[] = {12,21,5,351,98,122,39,87,53,90,17,68};
   	int size = sizeof(arr)/sizeof(arr[0]);
   	printArray(arr,size);
-  	QuickSort(arr,0,size-1);
+  	MergeSort(arr,0,size-1);
   	printArray(arr,size);
 	return 0;
 }
