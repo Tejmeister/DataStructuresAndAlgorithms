23b51ea20d90ac2b5f97f09a7fec69838d1d7dd5
Tejas Parmar <tejasparmar99@gmail.com>
2020-11-21 14:41:16 +0530

    Added solution for various traversals in binary tree

--- Trees/BinaryTree.cpp
+++ Trees/BinaryTree.cpp
@@ -12,7 +12,25 @@ struct TreeNode {
  	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 	TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  };
- 
+
+void printVector(vector<int> v){
+	vector<int>::iterator i;
+	for(i=v.begin(); i!=v.end(); i++){
+		cout<<*i<<" ";
+	}
+	cout<<endl;
+} 
+
+void printVector(vector<vector<int> > v){
+	
+	for(int i=0; i<v.size(); i++){
+		for(int j=0; j<v[i].size(); j++){
+			cout<<v[i][j]<<" ";
+		}
+		cout<<endl;
+	}
+	cout<<endl;
+} 
   
 /* function to create a new node of tree and  
 return pointer */
@@ -23,12 +41,195 @@ struct TreeNode* newNode(int key)
     return temp; 
 };
 
-void inorder(TreeNode *root){
+// Recursive Inorder Traversal
+void inorder(TreeNode *root, vector<int>& v){
 	if(root == NULL)
 		return;
-	inorder(root->left);
-	cout<<root->val<<" ";
-	inorder(root->right);
+	inorder(root->left, v);
+	v.push_back(root->val);
+	inorder(root->right, v);
+}
+
+// Iterative Inorder Traversal
+vector<int> inorder(TreeNode *root){
+	vector<int> result;
+
+	if(root == NULL)
+		return result;
+
+	stack<TreeNode*> s;
+
+	TreeNode *temp = root;
+	while(!s.empty() || temp!=NULL){
+		while(temp != NULL){
+			s.push(temp);
+			temp = temp->left;
+		}
+
+		temp = s.top();
+		s.pop();
+
+		result.push_back(temp->val);
+
+		temp = temp->right;
+	}
+
+	return result; 
+}
+
+// Recursive Preorder Traversal
+void preorder(TreeNode *root, vector<int>& v){
+	if(root == NULL)
+		return;
+	v.push_back(root->val);
+	preorder(root->left, v);
+	preorder(root->right, v);
+}
+
+// Iterative Preorder Traversal
+vector<int> preorder(TreeNode *root){
+	vector<int> result;
+
+	if(root == NULL)
+		return result;
+
+	stack<TreeNode*> s;
+	s.push(root);
+	TreeNode *temp = root;
+	while(!s.empty()){
+		temp = s.top();
+		s.pop();
+
+		result.push_back(temp->val);
+
+		if(temp->right)
+			s.push(temp->right);
+		if(temp->left)
+			s.push(temp->left);
+
+	}
+
+	return result; 
+}
+
+// Recursive Postorder Traversal
+void postorder(TreeNode *root, vector<int>& v){
+	if(root == NULL)
+		return;
+
+	postorder(root->left, v);
+	postorder(root->right, v);
+	v.push_back(root->val);
+}
+
+// Iterative Postorder Traversal
+vector<int> postorder(TreeNode *root){
+	vector<int> result;
+
+	if(root == NULL)
+		return result;
+
+	stack<TreeNode*> s;
+	s.push(root);
+	TreeNode *temp = root;
+	while(!s.empty()){
+		temp = s.top();
+		s.pop();
+
+		result.push_back(temp->val);
+
+		
+		if(temp->left)
+			s.push(temp->left);
+		if(temp->right)
+			s.push(temp->right);
+
+	}
+	reverse(result.begin(), result.end());
+	return result; 
+}
+
+// Level Order Traversal (BFS)
+vector<int> levelOrder(TreeNode *root){
+	vector<int> result;
+	if(root == NULL)
+		return result;
+	queue<TreeNode*> q;
+	TreeNode *temp;
+	q.push(root);
+	while(!q.empty()){
+		temp = q.front();
+		q.pop();
+		result.push_back(temp->val);
+		if(temp->left)
+			q.push(temp->left);
+		if(temp->right)
+			q.push(temp->right);
+	}
+
+	return result;
+}
+
+// Level order level by levl
+vector<vector<int> > levelOrderLevel(TreeNode *root){
+	vector<vector<int> > result;
+	if(root == NULL)
+		return result;
+	queue<TreeNode*> q;
+	TreeNode *temp;
+	q.push(root);
+	while(!q.empty()){
+		vector<int> t;
+		int s = q.size();
+		for(int i=0; i<s; i++){
+			temp = q.front();
+			q.pop();
+			t.push_back(temp->val);
+			//cout<<temp->val<<" ";
+			if(temp->left)
+				q.push(temp->left);
+			if(temp->right)
+				q.push(temp->right);
+		}
+		//cout<<endl;
+		result.push_back(t);
+	}
+
+	return result;
+}
+
+vector<int> zigzagLevelOrder(TreeNode *root){
+	vector<int> result;
+	if(root == NULL)
+		return result;
+	queue<TreeNode*> q;
+	TreeNode *temp;
+	q.push(root);
+	
+	bool isLevelOdd = false;
+	while(!q.empty()){
+		vector<int> t;
+		int s = q.size();
+		for(int i=0; i<s; i++){
+			temp = q.front();
+			q.pop();
+			t.push_back(temp->val);
+			//cout<<temp->val<<" ";
+			if(temp->left)
+				q.push(temp->left);
+			if(temp->right)
+				q.push(temp->right);
+		}
+		//cout<<endl;
+		if(isLevelOdd){
+		    reverse(t.begin(), t.end());
+		    
+		}
+		isLevelOdd = !isLevelOdd;
+        result.insert(result.end(), t.begin(), t.end());
+	}
+
+	return result;
 }
 
 void deleteTree(TreeNode* root){
@@ -63,15 +264,10 @@ int main(){
 						11				9
 					7		12		15		8
 	    */
+    vector<int> result;
+    vector<vector<int> > re;
+    result = zigzagLevelOrder(root);
+    printVector(result);
 
-    cout << "Inorder traversal before deletion : "; 
-    inorder(root); 
-
-    cout<< "\nCloning tree";
-    TreeNode *clone = deepCopy(root);
-    root->right->right->left = newNode(122); 
-	inorder(root);
-    cout << "\nTrying to print tree after deletion : "; 
-    inorder(clone);
 	return 0;
 }
